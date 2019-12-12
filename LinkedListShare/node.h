729759690f53103b76bc8db147b90d90ee04bd6c
Stefanene <stefanene@hotmail.com>
2019-12-12 10:36:46 -0800

    shareable part

---
+++ LinkedListShare/node.h
@@ -0,0 +1,22 @@
+#ifndef NODE_H
+#define NODE_H
+//#include "student.h"
+
+using namespace std;
+
+class Node {
+ public:
+  Node(Student*);  //constructor
+  ~Node();
+  //setters
+  void setStudent(Student*);
+  void setNext(Node*);
+  //getters
+  Student* getStudent();
+  Node* getNext();
+ private:
+  //variables
+  Student* student;
+  Node* next;
+};
+#endif        
